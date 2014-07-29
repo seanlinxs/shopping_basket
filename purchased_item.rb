1c1b341c405c2151a1e1fad7b89336ce74131ca9
Sean Lin <seanl@intersect.org.au>
2014-07-30 00:47:05 +1000

    Add more tests for PurchasedItem class Implement tax_exempt?

--- purchased_item.rb
+++ purchased_item.rb
@@ -10,4 +10,16 @@ class PurchasedItem
   def food?
     TaxClassifier::FOOD.include?(self.name)
   end
+
+  def book?
+    TaxClassifier::BOOKS.include?(self.name)
+  end
+
+  def medical_product?
+    TaxClassifier::MEDICAL_PRODUCTS.include?(self.name)
+  end
+
+  def tax_exempt?
+    food? || book? || medical_product?
+  end
 end
--- spec/purchased_item_spec.rb
+++ spec/purchased_item_spec.rb
@@ -14,9 +14,54 @@ describe PurchasedItem  do
   describe "#food?" do
     it "knows whether it is food" do
       TaxClassifier::FOOD.each do |f|
-        @food_item = PurchasedItem.new(1, f, 0.85)
-        expect(@food_item).to be_food
+        food_item = PurchasedItem.new(1, f, 0.85)
+        expect(food_item).to be_food
       end
     end
   end
+
+  describe "#book?" do
+    it "knows whether it is book" do
+      TaxClassifier::BOOKS.each do |b|
+        book_item = PurchasedItem.new(1, b, 0.85)
+        expect(book_item).to be_book
+      end
+    end
+  end
+
+  describe "#medical_product?" do
+    it "knows whether it is medical_product" do
+      TaxClassifier::MEDICAL_PRODUCTS.each do |m|
+        medical_product_item = PurchasedItem.new(1, m, 0.85)
+        expect(medical_product_item).to be_medical_product
+      end
+    end
+  end
+
+  describe "#tax_exempt?" do
+    context "is food" do
+      it "should be tax exempt" do
+        random_food_index = rand(TaxClassifier::FOOD.size)
+        food_item = PurchasedItem.new(1, TaxClassifier::FOOD[random_food_index], 0.85)
+        expect(food_item).to be_tax_exempt
+      end
+    end
+
+    context "is book" do
+      it "should be tax exempt" do
+        random_book_index = rand(TaxClassifier::BOOKS.size)
+        book_item = PurchasedItem.new(1, TaxClassifier::BOOKS[random_book_index], 0.85)
+        expect(book_item).to be_tax_exempt
+      end
+    end
+
+    context "is medical product" do
+      it "should be tax exempt" do
+        random_medical_product_index = rand(TaxClassifier::MEDICAL_PRODUCTS.size)
+        medical_product_item = PurchasedItem.new(1, TaxClassifier::MEDICAL_PRODUCTS[random_medical_product_index], 0.85)
+        expect(medical_product_item).to be_tax_exempt
+      end
+    end
+  end
+
 end
