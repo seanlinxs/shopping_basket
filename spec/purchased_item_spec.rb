8ffb8886f6f9464dd3757ab3cb39caff4612946b
Sean Lin <seanl@intersect.org.au>
2014-07-29 23:56:16 +1000

    Add tests for PurchasedItem class: new, is_food?

--- spec/purchased_item_spec.rb
+++ spec/purchased_item_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+describe PurchasedItem  do
+  before(:each) do
+    @purchased_item = PurchasedItem.new(qty: 1, name: "PurchasedItem", price: 12.49)
+  end
+
+  describe "#new" do
+    it "takes 3 arguments and return a PurchasedItem instance" do
+      @purchased_item.should be_instance_of(PurchasedItem)
+    end
+  end
+
+  describe "#is_food?" do
+    it "knows whether it is food" do
+      @food_item = PurchasedItem.new(qty: 1, name: "chocolate bar", price: 0.85)
+      @food_item.is_food?.should be_true
+    end
+  end
+end
